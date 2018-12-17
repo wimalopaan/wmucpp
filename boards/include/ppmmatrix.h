b2c2564fff61ef5f89703151d331c29796e204e4
Wilhelm <wilhelm.meier@fh-kl.de>
2018-12-17 09:39:07 +0100

    new board ppmmatrix

--- boards/include/ppmmatrix.h
+++ boards/include/ppmmatrix.h
@@ -27,6 +27,7 @@
 #include "mcu/avr/mcuppm.h"
 #include "mcu/avr/mcupwm.h"
 #include "mcu/avr/adc.h"
+#include "mcu/avr/cppm.h"
 #include "hal/constantrate.h"
 #include "hal/softspimaster.h"
 #include "hal/softppm.h"
@@ -44,18 +45,73 @@
 # include "util/memory.h"
 #endif
 
+namespace AVR {
+    template<typename A0, typename A1, typename A2, typename Signal, typename MCU = DefaultMcuType>
+    struct Multiplexer {
+        constexpr inline static void init() {
+            A0::template dir<AVR::Output>();
+            A1::template dir<AVR::Output>();
+            A2::template dir<AVR::Output>();
+            Signal::template dir<AVR::Output>();
+            A0::off();
+            A1::off();
+            A2::off();
+            Signal::off();
+        }
+        constexpr inline static void select(uintN_t<3> number) {
+            if (number & 0x01) {
+                A0::on();
+            }
+            else {
+                A0::off();
+            }
+            if (number & 0x02) {
+                A1::on();
+            }
+            else {
+                A1::off();
+            }
+            if (number & 0x04) {
+                A2::on();
+            }
+            else {
+                A2::off();
+            }
+        }
+        constexpr inline static void on() {
+            Signal::on();
+        }
+        constexpr inline static void off() {
+            Signal::off();
+        }
+    private:
+    };
+}
+
+
 using PortB = AVR::Port<DefaultMcuType::PortRegister, AVR::B>;
 using PortC = AVR::Port<DefaultMcuType::PortRegister, AVR::C>;
 using PortD = AVR::Port<DefaultMcuType::PortRegister, AVR::D>;
 using PortE = AVR::Port<DefaultMcuType::PortRegister, AVR::E>;
 
-using ledPin = AVR::Pin<PortD, 4>;
-using led = WS2812<9, ledPin, ColorSequenceGRB>;
-typedef led::color_type Color;
+using ppmInPin  = AVR::Pin<PortE, 2>;
+using ppmOutPin = AVR::Pin<PortB, 1>;
+
+using selectA0 = AVR::Pin<PortB, 0>;
+using selectA1 = AVR::Pin<PortD, 7>;
+using selectA2 = AVR::Pin<PortD, 6>;
+
+using multiplexer = AVR::Multiplexer<selectA0, selectA1, selectA2, ppmOutPin>;
+
+using paired   = AVR::Pin<PortD, 5>;
+
+using rxSelect = AVR::Pin<PortD, 3>;
+
+using cppm = AVR::CPPM<1, AVR::A, 8, ppmInPin>;
 
 using btUsart = AVR::Usart<0> ;
 using rcUsart = AVR::Usart<1, Hott::SumDProtocollAdapter<0>>;
 
-using systemClock = AVR::Timer8Bit<2>; // timer 2
-using alarmTimer = AlarmTimer<systemClock>;
+using systemClock = AVR::Timer8Bit<0>; 
+using alarmTimer = AlarmTimer<systemClock, UseEvents<false>>;
 
---
+++ boards/ppmmatrix/test02.cc
@@ -0,0 +1,49 @@
+#define NDEBUG
+
+#include "ppmmatrix.h"
+#include "console.h"
+
+using isrRegistrar = IsrRegistrar<rcUsart::RxHandler, rcUsart::TxHandler, btUsart::RxHandler, btUsart::TxHandler>;
+
+using terminalDevice = btUsart;
+using terminal = std::basic_ostream<terminalDevice>;
+
+int main() {
+    ppmInPin::dir<AVR::Output>();
+    
+    btUsart::init<9600>();
+    rcUsart::init<115200>();
+    {
+        Scoped<EnableInterrupt<>> ei;
+        uint8_t counter = 0;
+        while(true) {
+            ppmInPin::toggle();
+            
+            Util::delay(1000_ms);
+            std::outl<terminal>("Test03: "_pgm, ++counter);
+        }
+    }
+}
+
+// BT
+ISR(USART0_RX_vect) {
+    isrRegistrar::isr<AVR::ISR::Usart<0>::RX>();
+}
+ISR(USART0_UDRE_vect){
+    isrRegistrar::isr<AVR::ISR::Usart<0>::UDREmpty>();
+}
+
+// SumD
+ISR(USART1_RX_vect) {
+    isrRegistrar::isr<AVR::ISR::Usart<1>::RX>();
+}
+ISR(USART1_UDRE_vect){
+    isrRegistrar::isr<AVR::ISR::Usart<1>::UDREmpty>();
+}
+
+#ifndef NDEBUG
+void assertFunction(const PgmStringView& expr, const PgmStringView& file, unsigned int line) noexcept {
+    std::outl<terminal>("Assertion failed: "_pgm, expr, Char{','}, file, Char{','}, line);
+    while(true) {}
+}
+#endif
