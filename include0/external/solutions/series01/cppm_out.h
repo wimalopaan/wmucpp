381f6f56c9abfcc755d60d87e9a8b265a2eee054
wimalopaan <wilhelm.wm.meier@googlemail.com>
2021-04-25 12:07:16 +0200

    isr support for ccpm_out

--- include0/external/solutions/series01/cppm_out.h
+++ include0/external/solutions/series01/cppm_out.h
@@ -12,16 +12,18 @@ namespace External {
         using namespace External::Units::literals;
         using namespace std::literals::chrono;
         
-        template<typename TN, typename MCU = DefaultMcuType>
+        template<typename TN, typename Prescaler = std::integral_constant<uint8_t, 8>, 
+                 etl::Concepts::NamedFlag UseIsr = etl::NamedFlag<false>, typename MCU = DefaultMcuType>
         struct Cppm;
         
-        template<auto TN, typename Pos, AVR::Concepts::At01Series MCU>
-        struct Cppm<AVR::Portmux::Position<AVR::Component::Tca<TN>, Pos>, MCU> {
+        template<auto TN, typename Pos, typename Pre, typename UseIsr, AVR::Concepts::At01DxSeries MCU>
+        struct Cppm<AVR::Portmux::Position<AVR::Component::Tca<TN>, Pos>, Pre, UseIsr, MCU> {
+            inline static constexpr bool useIsr = UseIsr::value;
             using mcu_timer_t = typename MCU::TCA; 
             static constexpr auto mcu_tca = AVR::getBaseAddr<mcu_timer_t, TN>;
             using position = AVR::Portmux::Position<AVR::Component::Tca<TN>, Pos>;
 
-            static inline constexpr uint16_t prescaler = 8;
+            static inline constexpr uint16_t prescaler = Pre::value;
             static inline constexpr uint16_t period = (Project::Config::fMcu / prescaler) / 50_Hz;
 //            std::integral_constant<uint16_t, period>::_;
 
@@ -54,6 +56,27 @@ namespace External {
             
             using ca_t = mcu_timer_t::CtrlA_t;
             using cb_t = mcu_timer_t::CtrlB_t;
+            using ic_t = mcu_timer_t::Intctrl_t;
+
+            template<typename CB>
+            struct CmpHandler : AVR::IsrBaseHandler<AVR::ISR::Tca<0>::Cmp<1>> {
+                static inline void isr() {
+                    CB::once();
+                    mcu_tca()->intflags.template reset<mcu_timer_t::Intflags_t::cmp1>();
+                    ++mIndex;
+                    *mcu_tca()->perbuf = mFrames[mIndex].pulse;
+                    *mcu_tca()->cmp1buf = ccPulse;
+                }    
+            };
+            
+            template<typename CB>
+            struct OvfHandler : AVR::IsrBaseHandler<AVR::ISR::Tca<0>::Ovf> {
+                static inline void isr() {
+                    CB::once();
+                    mcu_tca()->intflags.template reset<mcu_timer_t::Intflags_t::ovf>();
+                }    
+            };
+            
             
             static inline constexpr auto pv = []{
                 for(auto p : mcu_timer_t::prescalerValues) {
@@ -64,12 +87,17 @@ namespace External {
             }();
             
             static inline void init() {
+                if constexpr(useIsr) {
+                    mcu_tca()->intctrl.template set<ic_t::ovf | ic_t::cmp1>();
+                }
                 mcu_tca()->ctrla.template set<pv | ca_t::enable>();
                 mcu_tca()->ctrlb.template set<cb_t::cmp1en | cb_t::pwm>();
                 *mcu_tca()->perbuf = ocMedium;
                 *mcu_tca()->cmp1buf = ccPulse;
                 mFrames[8].pulse = ocFrame - (8 * ocMedium);
             }
+
+            template<bool use = useIsr, typename = std::enable_if_not_t<use>> 
             static inline void periodic() {
                 mcu_tca()->intflags.template testAndReset<mcu_timer_t::Intflags_t::cmp1>([]{
                     ++mIndex;
@@ -77,6 +105,24 @@ namespace External {
                     *mcu_tca()->cmp1buf = ccPulse;
                 });
             }
+
+            template<bool use = useIsr, typename = std::enable_if_not_t<use>> 
+            static inline void periodic(auto f) {
+                mcu_tca()->intflags.template testAndReset<mcu_timer_t::Intflags_t::cmp1>([&]{
+                    ++mIndex;
+                    *mcu_tca()->perbuf = mFrames[mIndex].pulse;
+                    *mcu_tca()->cmp1buf = ccPulse;
+                    f();
+                });
+            }
+
+            template<bool use = useIsr, typename = std::enable_if_not_t<use>> 
+            static inline void onOvfl(auto f) {
+                mcu_tca()->intflags.template testAndReset<mcu_timer_t::Intflags_t::ovf>([&]{
+                    f();
+                });
+            }
+            
             static inline void set(const channel_t ch, const ranged_type& v) {
                 mFrames[ch].pulse = v;    
                 uint16_t sum = ocFrame;
