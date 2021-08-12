8515e5d0e60987272efbfdc1be87ac7981972eff
wimalopaan <wilhelm.wm.meier@googlemail.com>
2021-08-12 11:34:04 +0200

    add PD control

--- include0/etl/control.h
+++ include0/etl/control.h
@@ -5,6 +5,52 @@
 
 namespace Control {
     template<typename ValueType = uint16_t, typename T = etl::FixedPoint<int16_t, 10>>
+    struct PD {
+//        template<typename Stream, typename V, typename F> friend constexpr inline void out_impl(const PD<V, F>& pid);
+
+        using value_type = ValueType;
+        using error_type = std::make_signed_t<value_type>;
+        using factor_type = T;
+        
+        constexpr PD(const factor_type& p, const factor_type& d, const value_type& maxError, const value_type& maxCv) : 
+            pFactor(p), dFactor(d), maxError(maxError), maxCorrection{maxCv} {}
+        
+        constexpr inline error_type correctionValue(const value_type& actual, const value_type& setPoint) {
+            av = actual;
+            sp = setPoint;
+            const error_type error = std::clamp(actual - setPoint, -maxError, maxError);
+            ev = error;
+            const error_type pterm = error * pFactor;
+            pt = pterm;
+        
+            const error_type dterm = (actual - prevValue) * dFactor;    
+            prevValue = actual;
+            
+            if constexpr(!std::is_same_v<T, float>) {
+                return cv = std::clamp(pterm + dterm, -maxCorrection, maxCorrection);
+            }
+            else {
+                return cv = std::clamp((value_type)(pterm + dterm), -maxCorrection, maxCorrection);
+            }
+        }
+        constexpr inline void reset() {
+            prevValue = value_type{0};
+        }
+    private:
+        error_type cv{};
+        error_type pt{};
+        value_type prevValue{0};
+        value_type av{};
+        value_type sp{};
+        value_type ev{};
+        factor_type di{};
+        const factor_type pFactor;
+        const factor_type dFactor;
+        const value_type maxError{0};
+        const value_type maxCorrection{0};
+    };
+    
+    template<typename ValueType = uint16_t, typename T = etl::FixedPoint<int16_t, 10>>
     struct PID {
         template<typename Stream, typename V, typename F> friend constexpr inline void out_impl(const PID<V, F>& pid);
 
@@ -18,9 +64,9 @@ namespace Control {
         constexpr inline error_type correctionValue(const value_type& actual, const value_type& setPoint) {
             av = actual;
             sp = setPoint;
-            error_type error = std::clamp(actual - setPoint, -maxError, maxError);
+            const error_type error = std::clamp(actual - setPoint, -maxError, maxError);
             ev = error;
-            error_type pterm = error * pFactor;
+            const error_type pterm = error * pFactor;
             pt = pterm;
             di = iFactor * error;
         
@@ -44,9 +90,9 @@ namespace Control {
                     integral += di;
                     
                 }
-
             }
-            error_type dterm = (actual - prevValue) * dFactor;    
+
+            const error_type dterm = (actual - prevValue) * dFactor;    
             prevValue = actual;
             
             if constexpr(!std::is_same_v<T, float>) {
