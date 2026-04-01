6804f291f4a4cd391e12698cfe675082b4a2f704
wimalopaan <wilhelm.wm.meier@googlemail.com>
2026-04-01 11:08:54 +0200

    cleanup debug

--- include_stm32/debug_2.h
+++ include_stm32/debug_2.h
@@ -26,14 +26,15 @@ struct SerialBuffered {
     using pin = Config::pin;
     static inline void init() {
         uart::init();
-        if constexpr(Mcu::Stm::V4::detail::getSwap_v<Config>) {
-            static uint8_t af = Mcu::Stm::AlternateFunctions::mapper_v<pin, uart, Mcu::Stm::AlternateFunctions::RX>;
-            pin::afunction(af);
-        }
-        else {
-            static uint8_t af = Mcu::Stm::AlternateFunctions::mapper_v<pin, uart, Mcu::Stm::AlternateFunctions::TX>;
-            pin::afunction(af);
-        }
+        static constexpr uint8_t af = []{
+            if constexpr(Mcu::Stm::V4::detail::getSwap_v<Config>) {
+                return Mcu::Stm::AlternateFunctions::mapper_v<pin, uart, Mcu::Stm::AlternateFunctions::RX>;
+            }
+            else {
+                return Mcu::Stm::AlternateFunctions::mapper_v<pin, uart, Mcu::Stm::AlternateFunctions::TX>;
+            }
+        }();
+        pin::afunction(af);
     }
     static inline void reset() {
         uart::reset();
